93d956f7807385ee7e719996ece175c6ba3248c9
Jessica Minga <jcminga@utpl.edu.ec>
2017-05-25 02:27:24 -0500

    v3

--- public_html/internas/nav.php
+++ public_html/internas/nav.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" href="../css/bootstrap.min.css">
 <link rel="stylesheet" href="../css/menu.css">
-<link rel="stylesheet" href="../css/style.css">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
--- public_html/internas/navad.php
+++ public_html/internas/navad.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="../css/bootstrap.min.css">
 <link rel="stylesheet" href="../css/menu.css">
-<link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -20,12 +19,12 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     
-    
+
       <ul class="nav navbar-nav navbar-right">
        <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Login <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="login.php">Ingresar</a></li>
+            <li><a href="login.php">Salir</a></li>
            
           </ul>
         </li>
